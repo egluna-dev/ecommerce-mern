c5a6023ce76067b509d02a82d095451e55f58a93
egluna-dev <egluna@live.ca>
2022-09-18 23:23:58 +1000

    Added Navbar and react-bootstrap

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-r'>
+                    Copyright &copy; ProShop
+                </Col>
+            </Row>
+        </Container>
+        Footer
+    </footer>
+  )
+}
+
+export default Footer
