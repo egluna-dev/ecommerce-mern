83cd7277a691334d27fd1c05aaa5aee197bedada
egluna-dev <egluna@live.ca>
2022-09-29 23:21:14 +1000

    Changed header title to ProShop

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
             <Container>
                 <LinkContainer to='/'>
-                    <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand>ProShop</Navbar.Brand>
                 </LinkContainer>
                 
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
