b23849e60bbe568bedb9d648a046160e1f432591
egluna-dev <egluna@live.ca>
2022-09-30 19:32:41 +1000

    Created product details contants, reducer, and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer } from './reducers/productReducers';
 
 const reducer = {
-    productList: productListReducer
+    productList: productListReducer,
+    productDetails: productDetailsReducer
 }
 
 // const initialState = {}
