9b989cf48772a0971f7ec9743497d3e4f696a0b3
egluna-dev <egluna@live.ca>
2022-10-08 12:45:26 +1100

    Created user login action, constants, and reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,26 +2,31 @@ import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = {
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
-}
+    userLogin: userLoginReducer,
+};
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('cartItems')) : null;
 
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
-}
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
+};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
 const store = configureStore({
     reducer,
     initialState,
     middleware: middleware,
     devTools: process.env.NODE_ENV !== 'production',
-})
+});
 
-export default store
+export default store;
