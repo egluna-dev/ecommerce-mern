23194f52b57b23b14c45ec9d75cf6942a9b0fa96
egluna-dev <egluna@live.ca>
2022-10-10 23:04:12 +1100

    Created user register screen and alert messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
       <Header />
       <main className="py-5">
         <Container>
-          <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/login' component={LoginScreen}/>
+          <Route path='/register' component={RegisterScreen}/>
+          <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
           <Route exact path='/' component={HomeScreen}  />
         </Container>
